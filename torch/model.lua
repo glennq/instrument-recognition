c986b39d7bdb560c46c895c333af925c4de0b5e2
Glenn Qian <qianjy1990@gmail.com>
2015-03-18 12:17:35 -0400

    modified model back

--- torch/model.lua
+++ torch/model.lua
@@ -10,11 +10,11 @@ length = 44100
 ninputs = nfeats*length
 
 -- hidden units, filter sizes (for ConvNet only):
-nstates = {32,64,400}
-filtsize = {401, 425}
-poolsize = {8, 8}
-stridesize = {4, 4}
-viewsize = (((length - filtsize[1] - poolsize[1] + 1) / stridesize[1] + 1) - filtsize[2] - poolsize[2] + 1) / stridesize[2] + 1
+nstates = {64,64,400}
+filtsize = {401, 435}
+poolsize = {16, 12}
+stridesize = {8, 6}
+viewsize = 836
 
 print '==> construct model'
 
