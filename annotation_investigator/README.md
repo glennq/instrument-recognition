f39e319ba3226417391f2a558fce0bfaf7c39c38
Glenn Qian <qianjy1990@gmail.com>
2015-03-27 06:23:52 -0400

    added simple tool to investigate song labels

---
+++ annotation_investigator/README.md
@@ -0,0 +1,4 @@
+This is a simple tool for investigating the number of occurence each instrument in any given subset of all songs.
+
+
+To use it, simply run 'python webapp.py' from this directory, and then go to 'localhost:5000' or '127.0.0.1:5000' in your browser. In the form that appears, choose the grouping you want to use, and the songs you want to include (choose multiple songs by holding 'ctrl' or 'cmd'), and then click 'Check' to see the results.
---
+++ annotation_investigator/webapp.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template, flash
+from olform import *
+import numpy as np
+import pandas as pd
+import cPickle
+import os
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def inputData():
+    global song_instr_map
+    global grouping
+    form = ReportForm(request.form)
+    if request.method == 'POST' and form.validate():
+        songs = form.songs.data
+        group = int(form.group.data)
+        instr_cnt = {}
+        for i in songs:
+            counted = set()
+            for j in song_instr_map[i]:
+                try:
+                    instr = grouping[group][j] if j in grouping[group] else j
+                except Exception, e:
+                    print 'exception: {}'.format(e)
+                counted.add(instr)
+                if instr not in counted:
+                    instr_cnt[instr] = instr_cnt.get(instr, 0) + 1
+        flash('Intrument counts:')
+        for k, v in sorted(instr_cnt.items(), key=lambda x: x[1]):
+            flash('{}:\t{}'.format(k, v))
+    return render_template('form.html', form=form)
+
+if __name__ == '__main__':
+    with open('song_instr.pkl', 'rb') as f:
+        song_instr_map = cPickle.load(f)
+    groups = pd.read_csv(os.path.join(os.pardir, 'instGroup.csv'),
+                         index_col=0)
+    grouping = []
+    for i in range(1, groups.shape[1]):
+        grouping.append(dict(zip(groups['Instrument'].values,
+                                 groups['Group {}'.format(i)].values)))
+    app.secret_key = 'why would I tell you my secret k'
+    app.run()
