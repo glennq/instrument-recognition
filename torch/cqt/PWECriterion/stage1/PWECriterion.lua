4df2581cd78e624a05c71eb67612481c508da95d
Glenn Qian <qianjy1990@gmail.com>
2015-04-28 17:18:25 -0400

    fix

--- torch/cqt/PWECriterion/stage1/PWECriterion.lua
+++ torch/cqt/PWECriterion/stage1/PWECriterion.lua
@@ -10,16 +10,20 @@ function PWECriterion:updateOutput(input, target)
    -- BP-MLL, a convex surrogate loss for ranking loss
    local lsize = target:sum()
    local nlsize = target:size(1) - lsize
-   local mat = torch.zeros(lsize, nlsize):float()
-   input = input:float()
-   target = target:float()
-   for k = 1, mat:size(1) do
-      mat[k]:add(input[target:le(0.5)])
+   if lsize * nlsize == 0 then
+      self.output = 0
+   else
+      local mat = torch.zeros(lsize, nlsize):float()
+      input = input:float()
+      target = target:float()
+      for k = 1, mat:size(1) do
+         mat[k]:add(input[target:le(0.5)])
+      end
+      for k = 1, mat:size(2) do
+         mat[{{}, k}]:add(-1, input[target:ge(0.5)])
+      end
+      self.output = mat:exp():sum() / lsize / nlsize
    end
-   for k = 1, mat:size(2) do
-      mat[{{}, k}]:add(-1, input[target:ge(0.5)])
-   end
-   self.output = mat:exp():sum() / lsize / nlsize
    return self.output
 end
 
@@ -27,20 +31,24 @@ function PWECriterion:updateGradInput(input, target)
    local temp = input.new()
    local lsize = target:sum()
    local nlsize = target:size(1) - lsize
-   local mat = torch.zeros(lsize, nlsize):float()
-   input = input:float()
-   target = target:float()
-   for k = 1, mat:size(1) do
-      mat[k]:add(input[target:le(0.5)])
+   if lsize * nlsize == 0 then
+      self.output = 0
+   else
+      local mat = torch.zeros(lsize, nlsize):float()
+      input = input:float()
+      target = target:float()
+      for k = 1, mat:size(1) do
+         mat[k]:add(input[target:le(0.5)])
+      end
+      for k = 1, mat:size(2) do
+         mat[{{}, k}]:add(-1, input[target:ge(0.5)])
+      end
+      mat = mat:exp() / nlsize / lsize
+      self.gradInput = input.new()
+      self.gradInput:resizeAs(input)
+      self.gradInput[target:ge(0.5)] = -mat:sum(2)
+      self.gradInput[target:le(0.5)] = mat:sum(1)
+      self.gradInput = self.gradInput:typeAs(temp)
    end
-   for k = 1, mat:size(2) do
-      mat[{{}, k}]:add(-1, input[target:ge(0.5)])
-   end
-   mat = mat:exp() / nlsize / lsize
-   self.gradInput = input.new()
-   self.gradInput:resizeAs(input)
-   self.gradInput[target:ge(0.5)] = -mat:sum(2)
-   self.gradInput[target:le(0.5)] = mat:sum(1)
-   self.gradInput = self.gradInput:typeAs(temp)
    return self.gradInput
 end
