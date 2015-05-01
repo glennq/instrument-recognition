e30cdccc0cc4f41cac3bd286576f72e77c7dae93
Glenn Qian <qianjy1990@gmail.com>
2015-05-01 15:58:17 -0400

    fix

--- data/do_mfcc.py
+++ data/do_mfcc.py
@@ -11,7 +11,7 @@ def do_mfcc_d_d2(X, sr=44100):
         mfccs = librosa.feature.mfcc(y=y, sr=sr)
         delta1 = librosa.feature.delta(mfccs)
         delta2 = librosa.feature.delta(mfccs, order=2)
-        res.append(np.hstack([mfccs, delta1, delta2]))
+        res.append(np.vstack([mfccs, delta1, delta2]))
     return np.array(res, dtype='float32')
 
 
